7a8dd915b2a1808190e6c7f04678b60eba99e04e
Ian Campbell <ipcampbell@gmail.com>
2017-09-20 18:23:17 -0400

    Add itsreally.cool to the playbook.

---
+++ bin/itsreally.cool_production/secure-itsreally.cool-production.sh
@@ -0,0 +1,3 @@
+#! bin/bash
+
+ansible-playbook -i itsreally.cool_production security.yml --ask-vault-pass
---
+++ bin/itsreally.cool_production/setup-itsreally.cool-production.sh
@@ -0,0 +1,3 @@
+#! bin/bash
+
+ansible-playbook -i itsreally.cool_production site.yml -K --ask-vault-pass
